41f59d89b67231cc96a043e2af5a36c40a13daaf
sprocketsullivan <toelch@gmail.com>
2016-11-09 11:57:17 +0100

    clean up required... and excluded participants (3) cleaned up some code and messed up some code

--- fit_meta_dprime_to_data.R
+++ fit_meta_dprime_to_data.R
@@ -9,49 +9,70 @@ require(tidyverse) #require vs library
 require(stringr)
 require(data.table)
 require(dtplyr)  
-
+library(gridExtra)
 # Load functions and data files
 source('meta_dprime_functions.R')
 load("myData.RData")
 
+
+
 # preallocatoion
 fig<- list()
+fig.d<-list()
 m.in = data.frame()
-m.none = data.frame()
+m.d = data.frame()
+
 m.only = data.frame()
 m.same = data.frame()
 
 # RUN the analysis
 #participants 6 and 7 (i.e. 45524,45525) were excluded from all analyses already in the my.data. (they always chose 0 as their wager)
-# if you have them in my.data add:[-c(6:7)] 
+# if you have them in my.data add:[-c(7:8)] 
 
 
 # INCON
-pID <- unique(my.data$participant)
+pID <- unique(my.data$participant)[-c(1,7,8,12)]
 for (iparticipant in 1:length(pID)){
   f.data <- 
     filter(my.data,(!is.na(zConf)))%>%
     filter(social==0&participant==pID[iparticipant])
-  model <-DataMetaD(f.data)%>%
+    model <-DataMetaD(f.data)%>%
     FitMetaD()
-  m.in <- rbind(m.in,mean_se(model$meta_d))
+  m.in <- rbind(m.in,mean_se(as.numeric(model$meta_d)))
+  m.d<- rbind(m.d,mean_se(as.numeric(model$d1)))
 }
-fig[[1]]<-ggplot(aes(x=c(1:9),y=y,fill=c(1:9)),data=m.in) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("meta-dPrime") + xlab("participant") + ggtitle('INCON')
-
-
+m.in$participant<-pID
+m.d$participant<-pID
+fig[[1]]<-ggplot(aes(x=factor(participant),y=y),data=m.in) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("meta-dPrime") + xlab("participant") + ggtitle('INCON')+theme_classic()
+print(fig[[1]])
+fig.d[[1]]<-ggplot(aes(x=factor(participant),y=y),data=m.d) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("dPrime") + xlab("participant") + ggtitle('INCON')+theme_classic()
+m.in$diff<-m.in$y-m.d$y
+ggplot(aes(x=factor(participant),y=diff),data=m.in) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('INCON')+theme_classic()
 
 # NONE
-pID <- unique(my.data$participant)[-c(4,9)] # for excluded - RUNTIME ERROR: Failed check for discrete-valued parameters in function
+m.none = data.frame()
+m.none.d = data.frame()
+pID <- unique(my.data$participant)[-c(1,7,8,12)] # for excluded - RUNTIME ERROR: Failed check for discrete-valued parameters in function
 for (iparticipant in 1:length(pID)){
   f.data <- 
     filter(my.data,(!is.na(zConf)))%>%
-    filter(social==1&participant==pID[iparticipant]&norm2=="NONE")
+    filter(participant==pID[iparticipant]&norm2=="NONE")
   model <-DataMetaD(f.data)%>%
     FitMetaD()
   m.none <- rbind(m.none,mean_se(model$meta_d))
+  m.none.d<- rbind(m.none.d,mean_se(as.numeric(model$d1)))
 }
+m.none$participant<-pID
+m.none.d$participant<-pID
+fig[[2]]<-ggplot(aes(x=factor(participant),y=y),data=m.none) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("meta-dPrime") + xlab("participant") + ggtitle('NONE')+theme_classic()
+print(fig[[2]])
+fig.d[[2]]<-ggplot(aes(x=factor(participant),y=y),data=m.none.d) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("dPrime") + xlab("participant") + ggtitle('NONE')+theme_classic()
+grid.arrange(fig.d[[1]],fig.d[[2]])
+grid.arrange(fig[[1]],fig[[2]])
+m.in$diff<-m.in$y-m.d$y
+ggplot(aes(x=factor(participant),y=diff),data=m.in) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('INCON')+theme_classic()
 
-m.none <- rbind(m.none[1:3,], c(0,0,0), m.none[4:7,],c(0,0,0))
+
 rownames(m.none)<-c(1:9)
 fig[[2]]<-ggplot(aes(x=c(1:9),y=y,fill=c(1:9)),data=m.none) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("meta-dPrime") + xlab("participant") + ggtitle('NONE')
 
