cb9e25b0006ab1b3ec4d5c8c42463be27d65c236
Magdalena <majducha@users.noreply.github.com>
2016-11-10 13:32:22 +0100

    Cleaned up the code, excluded participants from the read_in_data added graphs with norm AND social info to the fit_meta_dprime

--- fit_meta_dprime_to_data.R
+++ fit_meta_dprime_to_data.R
@@ -1,108 +1,277 @@
-#########
-#fit data
-######### 
+################################################################
+# FIT META D' TO DATA
+################################################################
+
 # clear the workspace
 rm(list=ls())
 
+################################################################
+
+#### PRE-REQ
+
 # Import Packages (not sure if I need them all)
-require(tidyverse) #require vs library
-require(stringr)
-require(data.table)
-require(dtplyr)  
-library(gridExtra)
-# Load functions and data files
+library(tidyverse) 
+library(stringr)
+library(data.table)
+library(dtplyr)  
+library(gridExtra) # need to upload this one
+
+# Load functions
 source('meta_dprime_functions.R')
-load("myData.RData")
 
+# Load my.data (if it is not already present)
+if (!exists("my.data")){
+  load("confidenceData.RData")
+  print("loading my.data")
+}
 
-
-# preallocatoion
-fig<- list()
+# load participants' IDs
+pID <- unique(my.data$participant)
+  
+##### PRE-ALLOCATION
+# graphs
+fig.meta<- list()
 fig.d<-list()
-m.in = data.frame()
-m.d = data.frame()
+fig.d_meta <-list()
 
-m.only = data.frame()
-m.same = data.frame()
+# meta d' for all conditions
+m.meta <- matrix(NA, ncol = 4, nrow = 9)# set up so it corresponds with the number of p.
+colnames(m.meta)<-c("incon","none","only","same")
 
-# RUN the analysis
-#participants 6 and 7 (i.e. 45524,45525) were excluded from all analyses already in the my.data. (they always chose 0 as their wager)
-# if you have them in my.data add:[-c(7:8)] 
+# d' for all conditions
+m.d <- matrix(NA, ncol = 4, nrow = 9)
+colnames(m.d)<-c("incon","none","only","same")
 
+#####################################################################x
 
-# INCON
-pID <- unique(my.data$participant)[-c(1,7,8,12)]
-for (iparticipant in 1:length(pID)){
+# SIMPLE ANALYSIS
+
+#### INCON
+run.v <- c(1:length(pID))
+for (iparticipant in run.v){
   f.data <- 
     filter(my.data,(!is.na(zConf)))%>%
     filter(social==0&participant==pID[iparticipant])
-    model <-DataMetaD(f.data)%>%
+  model <-DataMetaD(f.data)%>%
     FitMetaD()
-  m.in <- rbind(m.in,mean_se(as.numeric(model$meta_d)))
-  m.d<- rbind(m.d,mean_se(as.numeric(model$d1)))
+  m.meta[iparticipant,1] <- mean(as.numeric(model$meta_d))
+  m.d[iparticipant,1] <- mean(as.numeric(model$d1))
 }
-m.in$participant<-pID
+
+
+#### NONE
+#part.exc <- c()
+run.v <- c(1:length(pID))
+for (iparticipant in run.v){
+  f.data <- 
+    filter(my.data,(!is.na(zConf)))%>%
+    filter(social!=0&participant==pID[iparticipant]&norm2=="NONE")
+  model <-DataMetaD(f.data)%>%
+    FitMetaD()
+  m.meta[iparticipant,2] <- mean(as.numeric(model$meta_d))
+  m.d[iparticipant,2] <- mean(as.numeric(model$d1))
+}
+
+#### ONLY
+part.exc <- 1# had to exclude 1 due to: RUNTIME ERROR: Failed check for discrete-valued parameters in function
+run.v <- c(1:length(pID))[-part.exc]
+for (iparticipant in run.v){
+  f.data <- 
+    filter(my.data,(!is.na(zConf)))%>%
+    filter(social!=0&participant==pID[iparticipant]&norm2=="ONLY")
+  model <- DataMetaD(f.data)%>%
+    FitMetaD()
+  m.meta[iparticipant,3] <- mean(as.numeric(model$meta_d))
+  m.d[iparticipant,3] <- mean(as.numeric(model$d1))
+}
+
+
+#### SAME
+run.v <- c(1:9) 
+for (iparticipant in run.v){
+  f.data <- 
+    filter(my.data,(!is.na(zConf)))%>%
+    filter(social!=0 &participant==pID[iparticipant]&norm2=="SAME")
+  model <-DataMetaD(f.data)%>%
+    FitMetaD()
+  m.meta[iparticipant,4] <- mean(as.numeric(model$meta_d))
+  m.d[iparticipant,4] <- mean(as.numeric(model$d1))
+}
+
+#### PREPARE FOR PLOTTING
+
+#convert matrices into data.frames (for ggplot, and easier handling)
+m.meta <- data.frame(m.meta)
+m.d <- data.frame(m.d)
+
+# add a column in m.meta for the difference between d' and meta-d' (metacognitive efficiency)
+m.meta$diff <- m.d-m.meta
+
+# add a column with participant info
+m.meta$participant<-pID
 m.d$participant<-pID
-fig[[1]]<-ggplot(aes(x=factor(participant),y=y),data=m.in) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("meta-dPrime") + xlab("participant") + ggtitle('INCON')+theme_classic()
-print(fig[[1]])
-fig.d[[1]]<-ggplot(aes(x=factor(participant),y=y),data=m.d) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("dPrime") + xlab("participant") + ggtitle('INCON')+theme_classic()
-m.in$diff<-m.in$y-m.d$y
-ggplot(aes(x=factor(participant),y=diff),data=m.in) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('INCON')+theme_classic()
 
-# NONE
-m.none = data.frame()
-m.none.d = data.frame()
-pID <- unique(my.data$participant)[-c(1,7,8,12)] # for excluded - RUNTIME ERROR: Failed check for discrete-valued parameters in function
-for (iparticipant in 1:length(pID)){
-  f.data <- 
+# PLOT AND PRINT
+#ADD 
+
+# Plot Incon
+fig.meta[[1]]<-ggplot(aes(x=factor(participant),y=as.numeric(incon)),data=m.meta) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('INCONGUENT')+theme_classic()+ ylim(-1.25, 1.25)
+fig.d[[1]]<-ggplot(aes(x=factor(participant),y=as.numeric(incon)),data=m.d) + geom_bar(stat = "identity")  + ylab("dPrime") + xlab("participant") + ggtitle('INCONGRUENT')+theme_classic() + ylim(0, 2.5)
+fig.d_meta[[1]]<-ggplot(aes(x=factor(participant),y=as.numeric(diff$incon)),data=m.meta) + geom_bar(stat = "identity")  + ylab("dPrime-metad") + xlab("participant") + ggtitle('INCONGUENT')+theme_classic()+ ylim(-0.5, 3.5)
+
+
+# Plot None
+fig.meta[[2]]<-ggplot(aes(x=factor(participant),y=as.numeric(none)),data=m.meta) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('NONE')+theme_classic()+ ylim(-1.25, 1.25)
+fig.d[[2]]<-ggplot(aes(x=factor(participant),y=as.numeric(none)),data=m.d) + geom_bar(stat = "identity")  + ylab("dPrime") + xlab("participant") + ggtitle('NONE')+theme_classic() + ylim(0, 2.5)
+fig.d_meta[[2]]<-ggplot(aes(x=factor(participant),y=as.numeric(diff$none)),data=m.meta) + geom_bar(stat = "identity")  + ylab("dPrime-metad") + xlab("participant") + ggtitle('NONE')+theme_classic()+ ylim(-0.5, 3.5)
+
+# plot Only
+fig.meta[[3]]<-ggplot(aes(x=factor(participant),y=as.numeric(only)),data=m.meta) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('ONLY')+theme_classic()+ ylim(-1.25, 1.25)
+fig.d[[3]]<-ggplot(aes(x=factor(participant),y=as.numeric(only)),data=m.d) + geom_bar(stat = "identity")  + ylab("dPrime") + xlab("participant") + ggtitle('ONLY')+theme_classic() + ylim(0, 2.5)
+fig.d_meta[[3]]<-ggplot(aes(x=factor(participant),y=as.numeric(diff$only)),data=m.meta) + geom_bar(stat = "identity")  + ylab("dPrime-metad") + xlab("participant") + ggtitle('ONLY')+theme_classic()+ ylim(-0.5, 3.5)
+
+# Plot SAME
+fig.meta[[4]]<-ggplot(aes(x=factor(participant),y=as.numeric(same)),data=m.meta) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('SAME')+theme_classic()+ ylim(-1.25, 1.25)
+fig.d[[4]]<-ggplot(aes(x=factor(participant),y=as.numeric(same)),data=m.d) + geom_bar(stat = "identity")  + ylab("dPrime") + xlab("participant") + ggtitle('SAME')+theme_classic() + ylim(0, 2.5)
+fig.d_meta[[4]]<-ggplot(aes(x=factor(participant),y=as.numeric(diff$same)),data=m.meta) + geom_bar(stat = "identity")  + ylab("dPrime-metad") + xlab("participant") + ggtitle('SAME')+theme_classic()+ ylim(-0.5, 3.5)
+
+#print(fig.meta[1:4])
+#print(fig.d[1:4])
+#print(fig.d_meta[1:4])
+
+grid.arrange(fig.meta[[1]],fig.meta[[2]],fig.meta[[3]],fig.meta[[4]], ncol=2, nrow =2)
+grid.arrange(fig.d[[1]],fig.d[[2]],fig.d[[3]],fig.d[[4]], ncol=2, nrow =2)
+grid.arrange(fig.d_meta[[1]],fig.d_meta[[2]],fig.d_meta[[3]],fig.d_meta[[4]], ncol=2, nrow =2)
+
+##########################################################################################x
+
+# Differences between social info in a norm
+
+#pre-allocation
+fig.meta.a<-list()
+fig.d.a<-list()
+fig.m_d.a <-list()
+
+m.advanced.meta <- matrix(NA, ncol = 6, nrow = 9)
+colnames(m.advanced.meta)<-c("noneValid","noneInvalid","onlyValid","onlyInvalid","sameValid","sameInvalid")
+
+m.advanced.d <- matrix(NA, ncol = 6, nrow = 9)
+colnames(m.advanced.d)<-c("noneValid","noneInvalid","onlyValid","onlyInvalid","sameValid","sameInvalid")
+
+# none with invalid social info
+run.v <- c(1:length(pID))
+for (iparticipant in run.v){
+  f.data <-
     filter(my.data,(!is.na(zConf)))%>%
-    filter(participant==pID[iparticipant]&norm2=="NONE")
+    filter(social==-1&participant==pID[iparticipant]&norm2=="NONE")
   model <-DataMetaD(f.data)%>%
     FitMetaD()
-  m.none <- rbind(m.none,mean_se(model$meta_d))
-  m.none.d<- rbind(m.none.d,mean_se(as.numeric(model$d1)))
+  m.advanced.meta[iparticipant,1] <- mean(as.numeric(model$meta_d))
+  m.advanced.d[iparticipant,1] <- mean(as.numeric(model$d1))
 }
-m.none$participant<-pID
-m.none.d$participant<-pID
-fig[[2]]<-ggplot(aes(x=factor(participant),y=y),data=m.none) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("meta-dPrime") + xlab("participant") + ggtitle('NONE')+theme_classic()
-print(fig[[2]])
-fig.d[[2]]<-ggplot(aes(x=factor(participant),y=y),data=m.none.d) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("dPrime") + xlab("participant") + ggtitle('NONE')+theme_classic()
-grid.arrange(fig.d[[1]],fig.d[[2]])
-grid.arrange(fig[[1]],fig[[2]])
-m.in$diff<-m.in$y-m.d$y
-ggplot(aes(x=factor(participant),y=diff),data=m.in) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('INCON')+theme_classic()
-
-
-rownames(m.none)<-c(1:9)
-fig[[2]]<-ggplot(aes(x=c(1:9),y=y,fill=c(1:9)),data=m.none) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("meta-dPrime") + xlab("participant") + ggtitle('NONE')
-
-
-# ONLY
-pID <- unique(my.data$participant) 
-for (iparticipant in 1:length(pID)){
-  f.data <- 
+# none with valid social info
+run.v <- c(1:length(pID))
+for (iparticipant in run.v){
+  f.data <-
     filter(my.data,(!is.na(zConf)))%>%
-    filter(social==1&participant==pID[iparticipant]&norm2=="ONLY")
+    filter(social==1 &participant==pID[iparticipant]&norm2=="NONE")
   model <-DataMetaD(f.data)%>%
     FitMetaD()
-  m.only <- rbind(m.only,mean_se(model$meta_d))
+  m.advanced.meta[iparticipant,2] <- mean(as.numeric(model$meta_d))
+  m.advanced.d[iparticipant,2] <- mean(as.numeric(model$d1))
 }
 
-fig[[3]]<-ggplot(aes(x=c(1:9),y=y,fill=c(1:9)),data=m.only) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("meta-dPrime") + xlab("participant") + ggtitle('SAME')
+# ONLY with invalid social info
+run.v <- c(2:length(pID)) #HAD TO EXCLUDE P.1 -> jags.model - COMPILATION ERROR on line 83(invalid range)
+for (iparticipant in run.v){
+  f.data <-
+    filter(my.data,(!is.na(zConf)))%>%
+    filter(social==-1 &participant==pID[iparticipant]&norm2=="ONLY")
+  model <-DataMetaD(f.data)%>%
+    FitMetaD()
+  m.advanced.meta[iparticipant,3] <- mean(as.numeric(model$meta_d))
+  m.advanced.d[iparticipant,3] <- mean(as.numeric(model$d1))
+}
+# ONLY with valid social info
+run.v <- c(2:length(pID)) # see above
+for (iparticipant in run.v){
+  f.data <-
+    filter(my.data,(!is.na(zConf)))%>%
+    filter(social==1 &participant==pID[iparticipant]&norm2=="ONLY")
+  model <-DataMetaD(f.data)%>%
+    FitMetaD()
+  m.advanced.meta[iparticipant,4] <- mean(as.numeric(model$meta_d))
+  m.advanced.d[iparticipant,4] <- mean(as.numeric(model$d1))
+}
 
+# same with invalid social info
+run.v <- c(1:length(pID))
+for (iparticipant in run.v){
+  f.data <-
+    filter(my.data,(!is.na(zConf)))%>%
+    filter(social==-1 &participant==pID[iparticipant]&norm2=="SAME")
+  model <-DataMetaD(f.data)%>%
+    FitMetaD()
+  m.advanced.meta[iparticipant,5] <- mean(as.numeric(model$meta_d))
+  m.advanced.d[iparticipant,5] <- mean(as.numeric(model$d1))
+}
 
-# SAME
-pID <- unique(my.data$participant)[-c(6:7)] 
-for (iparticipant in 1:length(pID)){
-  f.data <- 
+# same with valid social info
+part.exc <- c(7,8)# had to exclude 1 due to: RUNTIME ERROR: Failed check for discrete-valued parameters in function
+run.v <- c(1:length(pID))[-part.exc]
+for (iparticipant in run.v){
+  f.data <-
     filter(my.data,(!is.na(zConf)))%>%
     filter(social==1&participant==pID[iparticipant]&norm2=="SAME")
   model <-DataMetaD(f.data)%>%
     FitMetaD()
-  m.same <- rbind(m.same,mean_se(model$meta_d))
+  m.advanced.meta[iparticipant,6] <- mean(as.numeric(model$meta_d))
+  m.advanced.d[iparticipant,6] <- mean(as.numeric(model$d1))
 }
 
-m.same <- rbind(m.same[1:5,], c(0,0,0),c(0,0,0), m.same[ 6:7,] )
-rownames(m.same)<-c(1:9)
+#### PREPARE FOR PLOTTING
 
-fig[[4]]<-ggplot(aes(x=c(1:9),y=y,fill=c(1:9)),data=m.same) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin=ymin,ymax=ymax),width=0.2) + ylab("meta-dPrime") + xlab("participant") + ggtitle('ONLY')
+#convert matrices into data.frames (for ggplot, and easier handling)
+m.advanced.meta <- data.frame(m.advanced.meta)
+m.advanced.d <- data.frame(m.advanced.d)
+
+# add a column in m.meta for the difference between d' and meta-d' (metacognitive efficiency)
+m.advanced.meta$diff <- m.advanced.d-m.advanced.meta
+
+# add a column with participant info
+m.advanced.meta$participant<-pID
+m.advanced.d$participant<-pID
+
+# PLOT META INVALID, VALID
+fig.meta.a[[1]]<-ggplot(aes(x=factor(participant),y=as.numeric(noneInvalid)),data=m.advanced.meta) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('NONE-Invalid')+theme_classic() + ylim(-1.5, 1.5)
+fig.meta.a[[2]]<-ggplot(aes(x=factor(participant),y=as.numeric(noneValid)),data=m.advanced.meta) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('NONE-Valid')+theme_classic()+ ylim(-1.5, 1.5)
+grid.arrange(fig.meta.a[[1]],fig.meta.a[[2]], ncol=1, nrow =2)
+
+fig.meta.a[[3]]<-ggplot(aes(x=factor(participant),y=as.numeric(onlyInvalid)),data=m.advanced.meta) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('ONLY-Invalid')+theme_classic()+ ylim(-1.5, 1.5)
+fig.meta.a[[4]]<-ggplot(aes(x=factor(participant),y=as.numeric(onlyValid)),data=m.advanced.meta) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('ONLY-Valid')+theme_classic()+ ylim(-1.5, 1.5)
+grid.arrange(fig.meta.a[[3]],fig.meta.a[[4]], ncol=1, nrow =2)
+
+fig.meta.a[[5]]<-ggplot(aes(x=factor(participant),y=as.numeric(sameInvalid)),data=m.advanced.meta) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('SAME-Invalid')+theme_classic()+ ylim(-1.5, 1.5)
+fig.meta.a[[6]]<-ggplot(aes(x=factor(participant),y=as.numeric(sameValid)),data=m.advanced.meta) + geom_bar(stat = "identity") + ylab("meta-dPrime") + xlab("participant") + ggtitle('SAME-Valid')+theme_classic()+ ylim(-1.5, 1.5)
+grid.arrange(fig.meta.a[[5]],fig.meta.a[[6]], ncol=1, nrow =2)
+
+
+grid.arrange(fig.meta.a[[1]],fig.meta.a[[3]],fig.meta.a[[5]],fig.meta.a[[2]],fig.meta.a[[4]],fig.meta.a[[6]], ncol=3, nrow =2)
+
+# PLOT D INVALID, VALID
+fig.d.a[[1]]<-ggplot(aes(x=factor(participant),y=as.numeric(noneInvalid)),data=m.advanced.d) + geom_bar(stat = "identity") + ylab("dPrime") + xlab("participant") + ggtitle('NONE-Invalid')+theme_classic() + ylim(-1, 2.5)
+fig.d.a[[2]]<-ggplot(aes(x=factor(participant),y=as.numeric(noneValid)),data=m.advanced.d) + geom_bar(stat = "identity") + ylab("dPrime") + xlab("participant") + ggtitle('NONE-Valid')+theme_classic()+ ylim(-1, 2.5)
+grid.arrange(fig.d.a[[1]],fig.d.a[[2]], ncol=1, nrow =2)
+
+fig.d.a[[3]]<-ggplot(aes(x=factor(participant),y=as.numeric(onlyInvalid)),data=m.advanced.d) + geom_bar(stat = "identity") + ylab("dPrime") + xlab("participant") + ggtitle('ONLY-Invalid')+theme_classic()+ ylim(-1, 2.5)
+fig.d.a[[4]]<-ggplot(aes(x=factor(participant),y=as.numeric(onlyValid)),data=m.advanced.d) + geom_bar(stat = "identity") + ylab("dPrime") + xlab("participant") + ggtitle('ONLY-Valid')+theme_classic()+ ylim(-1, 2.5)
+grid.arrange(fig.d.a[[3]],fig.d.a[[4]], ncol=1, nrow =2)
+
+fig.d.a[[5]]<-ggplot(aes(x=factor(participant),y=as.numeric(sameInvalid)),data=m.advanced.d) + geom_bar(stat = "identity") + ylab("dPrime") + xlab("participant") + ggtitle('SAME-Invalid')+theme_classic()+ ylim(-1, 2.5)
+fig.d.a[[6]]<-ggplot(aes(x=factor(participant),y=as.numeric(sameValid)),data=m.advanced.d) + geom_bar(stat = "identity") + ylab("dPrime") + xlab("participant") + ggtitle('SAME-Valid')+theme_classic()+ ylim(-1, 2.5)
+grid.arrange(fig.d.a[[5]],fig.d.a[[6]], ncol=1, nrow =2)
+
+
+grid.arrange(fig.d.a[[1]],fig.d.a[[3]],fig.d.a[[5]],fig.d.a[[2]],fig.meta.a[[4]],fig.d.a[[6]], ncol=3, nrow =2)
+
+
--- read_in_data.R
+++ read_in_data.R
@@ -1,58 +1,135 @@
-#read in behavioural data
+#READ IN BEHAVIOURAL DATA and ANALYSE THEM
+
+# Input: This file needs to be in a folder that also contains a folder with the data (named data)
+# Output: This script outputs:
+#                  - data from all participants saved into a single file (my.data)
+#                  - Reaction times
+#                  - Accuracy
+#                  - Confidence based on accuracy, social info or norm
+
+
+#############################################################################x
+# clear the workspace
 rm(list=ls())
-require(tidyverse)
-require(stringr)
-require(data.table)
-require(dtplyr)
+
+# Import Packages
+library(tidyverse) 
+library(stringr)
+library(data.table)
+library(dtplyr)  
+
+# SET-UP variables
+colorScheme<-c("#F5A503","#56D9CD", "#3AA1BF") #yellow, l.blue, d.blue
+
+# participants to exclude
+part.excl <- c(7:8,12)
+#excludes pp45524,45525 (always 0 wager),62236(low accuracy - chance level), also suggested to exclude 25774 (tended to have low wager) 
+
+
+# Define pathways
+
+
+# Preallocation
 dodge <- position_dodge(.5)
 fig<- list() #container for figures
-colorScheme<-c("#F5A503","#56D9CD", "#3AA1BF")
+
+#########################################################################################
+
+######
+# Read in Data
+#####
+# Reads in the data, saves them into my.data, deletes trials with no response,wd and adds a couple of extra columns with labels
+
+
+# Extract data from Phase2 from all participants and save them into a single file: my.data
 setwd('.//data')
-files<-list.files(pattern='*Phase2.csv')
-my.data<-rbindlist(lapply(files, fread),use.names=TRUE,fill=TRUE)
+files<-list.files(pattern='*Phase2.csv')[-part.excl] 
+my.data<-rbindlist(lapply(files, fread),use.names=TRUE,fill=TRUE) 
 setwd('..\\')
-#delete lines at the end of each file denotet by NA
+
+
+# Consider removing unneccessary columns from my.data.
+
+
+# Delete lines at the end of each file denotet by NA 
+# (deletes the last trial, in which the outcome of the wager was caluculated)
 my.data<-my.data[!is.na(my.data$ttSAME)]
-#same for no responses
+
+# Delete trials in which participant failed to respond
 number_no_response<-nrow(my.data[is.na(my.data$key_resp_direction.rt)])
 my.data<-my.data[!is.na(my.data$key_resp_direction.rt)] #this leaves unequal numer of trials pPP
+
+# Calculate the number or participants
 no.part<-length(unique(my.data$participant))
+
+# Normalize confidence ratings and put them into a new column
+my.data[,zConf:=scale(as.numeric(PDW.response,na.rm=T)),by=participant]
+
+# Add a column that states if social info was presented
+my.data$si<-factor(ifelse(my.data$social==0,0,1),labels=c("no social\ninfo","social\ninfo"))
+
+# Add another column that states whether the social info was valid or not
+# Maybe exchange social2 for social 3?
+my.data$social2<-factor(my.data$social,labels = c("invalid\nsocial information","none","valid\nsocial information"))
+my.data$social3<-factor(my.data$social,labels=c("invalid","none","valid"))
+
+# Add column that states what norm was presented during each trial
+my.data$norm2<-ifelse(my.data$ttNONE,1,ifelse(my.data$ttSAME,2,3))
+my.data$norm2<-factor(my.data$norm,labels = c("NONE","SAME","ONLY"))
+
+
+
+
+#####
+#Save it
+###
+
+
+save(my.data,file='confidenceData.RData') # need to figure where to save it so that it is private and easy to access
+
+
+######################################################################################################
+
+#Put this into a new file
+
 ######
 #basic plots
 #####
-#### reaction times density plots #####
+
+### reaction times density plots ###
+
 #log RTs
+# Is this even used anywhere?
 my.data$rt.log<-log(my.data$key_resp_direction.rt)
 #calculate mean log RT for each participant
 my.data[,meanRT:=mean(rt.log,na.rm=T),by=participant]
 #z transform
 my.data$rt.log.z<-my.data$rt.log-my.data$meanRT
 # density distribution of RT in correct vs. wrong answers under NONE, SAME and ONLY conditions
-my.data$social2<-factor(my.data$social,labels = c("invalid\nsocial information","none","valid\nsocial information")) 
-my.data$norm2<-ifelse(my.data$ttNONE,1,ifelse(my.data$ttSAME,2,3))
-my.data$norm2<-factor(my.data$norm,labels = c("NONE","SAME","ONLY"))
-#################
-#correct answers overall
-############
+
+### correct answers overall ###
+
 #calculate some more variables
-#social info presnt
-my.data$si<-factor(ifelse(my.data$social==0,0,1),labels=c("no social\ninfo","social\ninfo"))
+
 summary_corr_choices<-
   group_by(my.data,participant,si,norm2)%>%
   summarise(mean_corr_l=mean(key_resp_direction.corr*100),N=length(key_resp_direction.corr))%>%
   group_by(si,norm2) %>%
   summarise(mean_corr=mean(mean_corr_l),sd_corr=sd(mean_corr_l))%>%
   mutate(se.min=mean_corr-sd_corr,se.max=mean_corr+sd_corr)
+
 ggplot(aes(y=mean_corr,x=norm2,fill=norm2),data=summary_corr_choices)+geom_bar(stat='identity',position="dodge")+
   geom_errorbar(aes(ymin=se.min,ymax=se.max),width=0.1,position=position_dodge(.9))+
   ylab("Proportion correct \ncompared to no social info")+
   xlab("")+theme_classic()+
   theme(text=element_text(size=20),legend.position=c(0.83,0.8),legend.title=element_blank(),legend.key.size = unit(2, 'lines'),legend.text=element_text(size=8))+
   geom_hline(yintercept=0,linetype="dashed")+
-  scale_fill_manual(values=c("#F5A503","#56D9CD", "#3AA1BF"))+
+  scale_fill_manual(values=c("#F5A503","#56D9CD", "#3AA1BF"))+ # isnt this the same as colourscheme at the beginning
   guides(fill = guide_legend(keywidth = 1.2, keyheight = 1.2))+facet_wrap(~si)
-my.data$si<-factor(ifelse(my.data$social==0,0,1),labels=c("no social\ninfo","social\ninfo"))
-my.data$social3<-factor(my.data$social,labels=c("invalid","none","valid"))
+
+
+
+
 ##########################
 #plot
 #correct choices
@@ -75,6 +152,7 @@ fig[[1]]<-ggplot(aes(y=meanCorr,x=norm2,fill=norm2),data=summary_corr_choices)+g
   scale_fill_manual(values=colorScheme)+
   guides(fill = guide_legend(keywidth = 1.2, keyheight = 1.2))+facet_wrap(~socialInfo2)
 print(fig[[1]])
+
 #plot
 #Reaction Time
 summary_RT<-
@@ -96,10 +174,13 @@ fig[[2]]<-ggplot(aes(y=meanCorr,x=norm2,fill=norm2),data=summary_RT)+geom_bar(st
   scale_fill_manual(values=colorScheme)+
   guides(fill = guide_legend(keywidth = 1.2, keyheight = 1.2))+facet_wrap(~socialInfo2)
 print(fig[[2]])
+
+
+
 #now confidence
 #calculate mean conf pp
 #first normalise per participant
-my.data[,zConf:=scale(as.numeric(PDW.response,na.rm=T)),by=participant]
+
 summary_confidence<-
   group_by(my.data,participant,social3,norm2)%>%
   summarise(meanCorrL=mean(zConf,na.rm=T))%>%
@@ -160,6 +241,7 @@ fig[[5]]<-ggplot(aes(y=meanCorr,x=norm3,fill=norm3),data=summary_confidence_cor)
 print(fig[[5]])
 #do players follow social information effect on confidence
 my.data$follow<-my.data$key_resp_direction.corr*my.data$social
+my.data$norm3
 summary_confidence_cor<-
   filter(my.data,norm3!="incong")%>%
   group_by(participant,follow,norm3)%>%
@@ -176,7 +258,4 @@ fig[[6]]<-ggplot(aes(y=meanCorr,x=norm3,fill=norm3),data=summary_confidence_cor)
   scale_fill_manual(values=c(colorScheme,"grey"))+
   guides(fill = guide_legend(keywidth = 1.2, keyheight = 1.2))+facet_wrap(~follow)
 print(fig[[6]])
-
-
-
 
